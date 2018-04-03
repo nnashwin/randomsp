695a97257e92dc35021dfadadca7be6f4438887e
Tyler Boright <lupin3.ken@gmail.com>
2018-04-03 16:57:52 -0700

    making an http request to get the correct wiki article of the s&p stocks

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/html"
+	// "golang.org/x/net/html"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("vim-go")
+	resp, _ := http.Get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println("HTML:\n\n", string(bytes))
+
+	resp.Body.Close()
 }
