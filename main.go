38d4a81def7662b98f2edc850cbd1b632cd3fdac
Tyler Boright <lupin3.ken@gmail.com>
2018-04-03 22:53:50 -0700

    adds functionality to grab the table from the S&P 500 wikipedia article

--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"golang.org/x/net/html"
+	"log"
 	"net/http"
+	"strings"
 )
 
+func getFirstElementByClassName(className string, n *html.Node) (element *html.Node, ok bool) {
+	for _, a := range n.Attr {
+		if a.Key == "class" && strings.Contains(a.Val, className) {
+			return n, true
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if element, ok = getFirstElementByClassName(className, c); ok {
+			return
+		}
+	}
+	return
+}
+
 func main() {
-	fmt.Println("vim-go")
+
+	response, err := http.Get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	root, err := html.Parse(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	el, ok := getFirstElementByClassName("wikitable", root)
+	if !ok {
+		log.Fatal(err)
+	}
+
+	log.Printf("%+v", el)
 }
