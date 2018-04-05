76c50c60cb74d42adb97c7168ad4a53747e408e6
Tyler Boright <lupin3.ken@gmail.com>
2018-04-05 16:33:56 -0700

    adds two tests for the pseudo-random functions in the randomsp package

---
+++ randomsp_test.go
@@ -0,0 +1,28 @@
+package randomsp
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestGetRandomInt(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	int1 := GetRandomInt(1, 1000000)
+	int2 := GetRandomInt(1, 1000000)
+	if int1 == int2 {
+		t.Error("the GetRandomInt ints should not equal each other")
+	}
+}
+
+func TestGetRandomString(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	str := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	ss := strings.Split(str, "")
+
+	if GetRandomString(ss) == GetRandomString(ss) {
+		t.Error("The Get Rand String function should get different strings from the slice")
+	}
+}
