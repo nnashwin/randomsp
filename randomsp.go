5505b3782f5d8258674a33b77749fcc263dc9d7c
Tyler Boright <lupin3.ken@gmail.com>
2018-04-08 11:45:57 -0700

    changes the order of the functions in the library

--- randomsp.go
+++ randomsp.go
@@ -9,58 +9,6 @@ import (
 	"time"
 )
 
-func getStandardPoorsStocks() (stocks []string) {
-	res, err := http.Get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tbody := doc.Find("tbody").First()
-	tbody.Find("tr").Each(func(i int, s *goquery.Selection) {
-		td := s.Find("td:first-child")
-		stocks = append(stocks, td.Text())
-	})
-	return
-}
-
-func getNasdaqStocks() (stocks []string) {
-	res, err := http.Get("https://en.wikipedia.org/wiki/NASDAQ-100")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	col := doc.Find(".column-count-2").First()
-	col.Find("li").Each(func(i int, s *goquery.Selection) {
-		str := s.Text()
-		idx1 := strings.Index(str, "(")
-		idx2 := strings.Index(str[idx1:], ")")
-		stocks = append(stocks, str[idx1+1:idx2+idx1])
-	})
-	return
-}
-
 func getFinancialTimesStocks() (stocks []string) {
 	res, err := http.Get("https://en.wikipedia.org/wiki/FTSE_100_Index")
 	if err != nil {
@@ -111,6 +59,58 @@ func getItalianFinancialTimesStocks() (stocks []string) {
 	return
 }
 
+func getNasdaqStocks() (stocks []string) {
+	res, err := http.Get("https://en.wikipedia.org/wiki/NASDAQ-100")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	col := doc.Find(".column-count-2").First()
+	col.Find("li").Each(func(i int, s *goquery.Selection) {
+		str := s.Text()
+		idx1 := strings.Index(str, "(")
+		idx2 := strings.Index(str[idx1:], ")")
+		stocks = append(stocks, str[idx1+1:idx2+idx1])
+	})
+	return
+}
+
+func getStandardPoorsStocks() (stocks []string) {
+	res, err := http.Get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tbody := doc.Find("tbody").First()
+	tbody.Find("tr").Each(func(i int, s *goquery.Selection) {
+		td := s.Find("td:first-child")
+		stocks = append(stocks, td.Text())
+	})
+	return
+}
+
 func getRandomInt(min, max int) int {
 	return min + rand.Intn(max-min)
 }
@@ -118,18 +118,6 @@ func getRandomInt(min, max int) int {
 func getRandomString(ss []string) string {
 	randInt := getRandomInt(1, len(ss))
 	return ss[randInt:(randInt + 1)][0]
-}
-
-func GetRandomSPStock() string {
-	rand.Seed(time.Now().UnixNano())
-	stockSlice := getStandardPoorsStocks()
-	return getRandomString(stockSlice)
-}
-
-func GetRandomNasdaqStock() string {
-	rand.Seed(time.Now().UnixNano())
-	stockSlice := getNasdaqStocks()
-	return getRandomString(stockSlice)
 }
 
 func GetRandomFinancialTimesStock() string {
@@ -148,4 +136,16 @@ func GetRandomIndexStock() string {
 	rand.Seed(time.Now().UnixNano())
 	stockFuncs := []func() string{GetRandomNasdaqStock, GetRandomSPStock, GetRandomFinancialTimesStock, GetRandomItalianFinancialTimesStock}
 	return stockFuncs[getRandomInt(0, len(stockFuncs))]()
+}
+
+func GetRandomNasdaqStock() string {
+	rand.Seed(time.Now().UnixNano())
+	stockSlice := getNasdaqStocks()
+	return getRandomString(stockSlice)
+}
+
+func GetRandomSPStock() string {
+	rand.Seed(time.Now().UnixNano())
+	stockSlice := getStandardPoorsStocks()
+	return getRandomString(stockSlice)
 }
