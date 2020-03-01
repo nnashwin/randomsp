9245acfcf40ad37832a94994b9872e7f97d6207a
ru-lai <ru.lai.development@gmail.com>
2020-03-01 06:10:59 -0800

    adds an integration test to ensure that the stock functions are performing correctly

--- randomsp_test.go
+++ randomsp_test.go
@@ -2,12 +2,13 @@ package randomsp
 
 import (
 	"math/rand"
+	"regexp"
 	"strings"
 	"testing"
 	"time"
 )
 
-func TestGetRandomInt(t *testing.T) {
+func TestUnitGetRandomInt(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	int1 := getRandomInt(1, 1000000)
 	int2 := getRandomInt(1, 1000000)
@@ -16,12 +17,89 @@ func TestGetRandomInt(t *testing.T) {
 	}
 }
 
-func TestGetRandomString(t *testing.T) {
+func TestUnitGetRandomString(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	str := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	ss := strings.Split(str, "")
 
 	if getRandomString(ss) == getRandomString(ss) {
 		t.Error("The Get Rand String function should get different strings from the slice")
+	}
+}
+
+type testStockIndex struct {
+	IndexName string
+	Stocks    []string
+}
+
+// a test to hit all stock data sources and verify that acronyms are returned
+func TestIntegrationAcronymsReturned(t *testing.T) {
+	var stocks []testStockIndex
+
+	dax, err := getDaxStocks()
+	if err != nil {
+		t.Fatalf("acronym integration test failed with the following error: %s", err)
+	}
+
+	daxIdx := testStockIndex{
+		"DAX",
+		dax,
+	}
+
+	ft, err := getFinancialTimesStocks()
+	if err != nil {
+		t.Fatalf("acronym integration test failed with the following error: %s", err)
+	}
+
+	ftIdx := testStockIndex{
+		"FinancialTimes",
+		ft,
+	}
+
+	ift, err := getItalianFinancialTimesStocks()
+	if err != nil {
+		t.Fatalf("acronym integration test failed with the following error: %s", err)
+	}
+
+	iftIdx := testStockIndex{
+		"ItalianFinancialTimes",
+		ift,
+	}
+
+	n, err := getNasdaqStocks()
+	if err != nil {
+		t.Fatalf("acronym integration test failed with the following error: %s", err)
+	}
+
+	nIdx := testStockIndex{
+		"Nasdaq",
+		n,
+	}
+
+	sp, err := getStandardPoorsStocks()
+	if err != nil {
+		t.Fatalf("acronym integration test failed with the following error: %s", err)
+	}
+
+	spIdx := testStockIndex{
+		"StandardPoor",
+		sp,
+	}
+
+	stocks = append(stocks, daxIdx)
+	stocks = append(stocks, ftIdx)
+	stocks = append(stocks, iftIdx)
+	stocks = append(stocks, nIdx)
+	stocks = append(stocks, spIdx)
+
+	// Create Regex To Test that inputs have sequences of capital letters (prove that it is a stock quote acryonym
+	stockRegex := regexp.MustCompile("[A-Z]+")
+
+	for _, idxSlice := range stocks {
+		for _, stock := range idxSlice.Stocks {
+			if stockRegex.FindStringIndex(stock) == nil {
+				t.Fatalf("the stock returned does not match the stock regex and is probably not a stock.  \nIndex: %q\nStock: %q", idxSlice.IndexName, stock)
+			}
+		}
 	}
 }
